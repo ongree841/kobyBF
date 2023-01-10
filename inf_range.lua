3654c30f4027e6bf13bfce2fdc60dbb673155fb5
ongree841 <48439854+ongree841@users.noreply.github.com>
2023-01-10 19:29:17 +0700

    Update inf_range.lua

--- inf_range.lua
+++ inf_range.lua
@@ -1,6 +1,6 @@
 local units = game:GetService("Workspace")["_UNITS"]
 ['dio'].HumanoidRootPart.CFrame = enemi.HumanoidRootPart.CFrame
-['yuno']HumanoidRootPart.CFrame = enemi.HumanoidRootPart.CFrame
+['yono']HumanoidRootPart.CFrame = enemi.HumanoidRootPart.CFrame
 ['Ice Queen']HumanoidRootPart.CFrame = enemi.HumanoidRootPart.CFrame
 ['Puchi']HumanoidRootPart.CFrame = enemi.HumanoidRootPart.CFrame
 ['Jio']HumanoidRootPart.CFrame = enemi.HumanoidRootPart.CFrame
