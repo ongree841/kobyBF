4da17edde4df68179a2f1a29f473582dc42c0455
ongree841 <48439854+ongree841@users.noreply.github.com>
2023-01-10 17:09:30 +0700

    Update inf_range.lua

--- inf_range.lua
+++ inf_range.lua
@@ -1,3 +1,6 @@
 lua game:GetService('Workspace')['_UNITS']
 ['dio'].HumanoidRootPart.CFrame = enemi.HumanoidRootPart.CFrame
 ['yuno']HumanoidRootPart.CFrame = enemi.HumanoidRootPart.CFrame
+['Ice Queen']HumanoidRootPart.CFrame = enemi.HumanoidRootPart.CFrame
+['Puchi']HumanoidRootPart.CFrame = enemi.HumanoidRootPart.CFrame
+['Jio']HumanoidRootPart.CFrame = enemi.HumanoidRootPart.CFrame
